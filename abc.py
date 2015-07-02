0b566c8b0d8689e8cb8202b3cfa4e7d340c3fb8c
AmerGit <ameralameddine@gmail.com>
2015-07-02 14:49:57 -0400

    Update abc.py

--- abc.py
+++ abc.py
@@ -14,48 +14,53 @@ def getMayaMainPtr():
 
 class nHairToolset(QtGui.QMainWindow):
     def __init__(self, windowId, windowTitle, windowDimensions):
+
         # initialize main window features
         super(nHairToolset, self).__init__(getMayaMainPtr())
         self.setObjectName(windowId)
         self.setWindowTitle(windowTitle)
         self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
         self.setFixedSize(windowDimensions[0], windowDimensions[1])
+
         # style setup
         self.style = QtGui.QStyleFactory.create('plastique')
         self.setStyle(self.style)
+
         # add layout and widgets
         centerWidget = QtGui.QWidget(self)
         self.setCentralWidget(centerWidget)
 
-        # main layout for central widget
+        # setup central widget to hold all elements
         centerWidget.setLayout(QtGui.QVBoxLayout(centerWidget))
         centerWidget.layout().setContentsMargins(0,0,0,0)
-        # create inner tab
-        innerTabs= QtGui.QTabWidget(centerWidget)
+
+        # create the tabwidget to hold the tabs
+        innerTabs = QtGui.QTabWidget()
         innerTabs.setFocusPolicy(QtCore.Qt.NoFocus)
         centerWidget.layout().addWidget(innerTabs)
 
         # first Tab
-        mainControlsTab = QtGui.QWidget(innerTabs)
+        mainControlsTab = QtGui.QWidget()
         innerTabs.addTab(mainControlsTab, 'Main Functions')
-        mainControlsLayout = QtGui.QVBoxLayout(mainControlsTab)
+        mainControlsLayout = QtGui.QVBoxLayout()
         mainControlsLayout.setSpacing(2)
         mainControlsLayout.setAlignment(QtCore.Qt.AlignTop)
         mainControlsTab.setLayout(mainControlsLayout )
+
         # rigFix options
         rigFxGrpBox = customGroupBox('RigFx Options', (180,150), 'plastique', 0, 0)
-
         mainControlsLayout.addWidget((rigFxGrpBox))
         rigFixNameField = self.createLabeledNameField('Name: ', 'Enter a RigFx name...')
         buildRigFxBtn = QtGui.QPushButton('Build RigFx')
         updateSetsBtn = QtGui.QPushButton('Update Sets')
         motionMultBtn = QtGui.QPushButton('MotionMultiplier')
-
+        # populate rigFx groupBox
         rigFxGrpBox.layout().addLayout(rigFixNameField)
         rigFxGrpBox.layout().addWidget(buildRigFxBtn)
         rigFxGrpBox.layout().addWidget(updateSetsBtn)
         rigFxGrpBox.layout().addWidget(motionMultBtn)
         rigFxGrpBox.layout().addStretch(True)
+
         # second tab
         secondaryControlsTab = QtGui.QWidget(innerTabs)
         innerTabs.addTab(secondaryControlsTab, 'Secondary')
