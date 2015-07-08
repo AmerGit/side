27f27735faad137ad41318b96d032d8c3efc1077
AmerGit <ameralameddine@gmail.com>
2015-07-08 16:46:49 -0400

    Update abc.py

--- abc.py
+++ abc.py
@@ -1,7 +1,7 @@
 """
-Class used to create and setup the UI for the nHair RigFx toolset. The majortiy of the heavy maya tasks that UI will
-take care of will be handled in a separate file (nHairUtils) in order to separate the design/layout and UI functionality
-from the core functionality.
+    Class used to create and setup the UI for the nHair RigFx toolset. The majortiy of the heavy maya tasks that UI will
+    take care of will be handled in a separate file (nHairUtils) in order to separate the design/layout and UI functionality
+    from the core functionality.
 """
 from maya import cmds, OpenMaya, OpenMayaUI
 from PySide import QtGui, QtCore
